e738052ac939a0a5a213a1d1197fb8434733972c
henchickenhead <henchickenhead@icloud.com>
2025-12-09 20:04:32 +0000

    Added a check for when the aliens are touching the edge. Also integrated the shooting mechanics. The aliens randomly fire from the bottom row as much as 4 bullets on the screen at once. They can also get shot as it runs a check to ensure that the position of the bullet is the same as one of the aliens

--- aliens.py
+++ aliens.py
@@ -1,4 +1,6 @@
 import pygame as game
+import random
+
 
 class Alien(game.sprite.Sprite): 
     def __init__(self, pos, size=40):
@@ -7,6 +9,20 @@ class Alien(game.sprite.Sprite):
         self.image.fill((0,255,0))
         self.rect = self.image.get_rect(topleft=pos) 
     
+class Bullet(game.sprite.Sprite):
+    def __init__(self, direction, xpos, ypos, speed=5, color=(255,255,0)):
+        super().__init__()
+        self.direction = direction
+        self.speed = speed
+        size = 7
+        self.image = game.Surface((size, size))
+        self.image.fill(color)
+        self.rect = self.image.get_rect(center=(xpos, ypos))
+
+    def move(self):
+        dy = -self.speed if self.direction else self.speed
+        self.rect.y += dy
+
 AlienIconPattern1 = [
     [0,0,1,0,0,0,0,1,0,0],
     [0,1,0,1,0,0,1,0,1,0],
@@ -56,22 +72,21 @@ AlienIconPattern4 = [
     [1,0,1,0,1,1,0,1,0,1]
 ]
 
-AlienBulletPattern = [
-    [0,1,0],
-    [0,1,0],
-    [0,1,0]
-]
 
 class AlienAbility:
     def __init__(self, pattern, xpos, ypos):
         self.speed = 1
-        self.isAtBottom = False
+        self.isAlive = True
+        self.direction = True
+        self.isAtEdge = False
         self.tiles = game.sprite.Group() 
         self.tile_size = 3
         self.pattern = pattern
-        self.bullet = AlienBulletPattern
         self.xpos = xpos
         self.ypos = ypos
+        self.score = 100
+        self.bullet = Bullet
+        self.count = 0
         
 
     def build(self):
@@ -86,20 +101,64 @@ class AlienAbility:
                     self.tiles.add(tile)
 
     def move(self):
-        pass
+        if self.direction:
+            dx = self.speed * 5 
+        else:
+            dx = -self.speed * 5
+        self.xpos += dx
+        for tile in self.tiles:
+            tile.rect.x += dx
+    
+    def dropDown(self):
+        self.ypos += 10 + self.tile_size * 10   
+        for tile in self.tiles:
+            tile.rect.y += 10 + self.tile_size * 10
+        self.direction = not self.direction
+
+    def shoot(self) -> Bullet:
+        # create and return a Bullet instance positioned at this alien
+        self.count += 1
+        bx = int(self.xpos + (self.tile_size * len(self.pattern[0])) // 2)
+        by = int(self.ypos + (self.tile_size * len(self.pattern) ) )
+        return Bullet(False, bx, by)
+
+
+
+
+
 
 class Aliens():
-    def __init__(self, xpos =int , ypos = int ):
-        self.xpos = xpos
-        self.ypos = ypos
-        self.alienClass = Alien((xpos, ypos))
+    def __init__(self ):
+        self.screenDetails = game.display.Info().current_w, game.display.Info().current_h
+        self.xpos = 100
+        self.ypos = 100
+        self.alienClass = Alien((self.xpos, self.ypos))
         self.direction = True
-        self.isBorderLeft = False
-        self.isBorderRight = False
+        self.isBorder = False
+        self.canMove = True
+        self.isShot = False
+        # central bullets group for all alien bullets
+        self.bullets = game.sprite.Group()
         self.createAliens()
+        
+    def borderCheck(self):
+        for alienRow in self.alienArray:
+            for alien in alienRow:
+                if alien.xpos <= 100 or alien.xpos >= game.display.Info().current_w - 100:
+                    print("Edge reached")
+                    self.isBorder = True
+                if alien.ypos >= game.display.Info().current_h - 175:
+                    self.canMove = False
+                    print("Aliens have landed!")
+        if self.isBorder:
+            for alienRow in self.alienArray:
+                for alien in alienRow:
+                    alien.dropDown()
+                    alien.move()
+            self.isBorder = False
+
 
     def createAliens(self):
-        # create a 2D list (rows x cols) of AlienAbility instances
         rows = 5
         cols = 11
         self.alienArray = []
@@ -107,7 +166,7 @@ class Aliens():
         spacing = 10 + temp.tile_size * 10
         for i in range(rows):
             row = []
-            if i <= 2:
+            if i < 1:
                 for j in range(cols):
                     xpos = self.xpos + j * spacing
                     ypos = self.ypos + i * spacing
@@ -115,7 +174,7 @@ class Aliens():
                     alien_ability.build()
                     row.append(alien_ability)
                 self.alienArray.append(row)
-            elif i >2 or i <=4:
+            elif i >=1 and i <3:
                 for j in range(cols):
                     xpos = self.xpos + j * spacing
                     ypos = self.ypos + i * spacing
@@ -134,18 +193,65 @@ class Aliens():
         return self.alienArray
     
     def drawAliens(self):
+        surface = game.display.get_surface()
         for alienRow in self.alienArray:
             for alien in alienRow:
-                alien.build()
-                alien.tiles.draw(game.display.get_surface())
+                alien.tiles.draw(surface)
+        for b in list(self.bullets):
+            b.move()
+            if b.rect.bottom < 0 or b.rect.top > surface.get_height():
+                b.kill()
+        self.bullets.draw(surface)
+
+    def groupMove(self, counter=None):
+        if self.canMove:
+            for alienRow in self.alienArray:
+                for alien in alienRow:
+                    if counter is None:
+                        alien.move()
+                    else:
+                        period = max(1, int(20 / alien.speed))
+                        if counter % period == 0:
+                            alien.move()
+                    if random.randint(0, 1000) % 100 == 0:
+                        self.shoot()
+    
+    def getBullets(self):
+        return self.bullets
 
     def shoot(self):
-        pass
+        if len(self.bullets.sprites()) > 3:
+            return
+        cols = len(self.alienArray[0])
+        attempts = 0
+        while attempts < 10:
+            col = random.randint(0, cols - 1)
+            shooter = None
+            for row in reversed(self.alienArray):
+                a = row[col]
+                if getattr(a, 'isAlive', True):
+                    shooter = a
+                    break
+            if shooter:
+                b = shooter.shoot()
+                if b:
+                    self.bullets.add(b)
+                    return
+            attempts += 1
 
-    def getShot(self):
-        pass    
-# next steps
+    def getShot(self, bulletPosition):
+        for alienRow in self.alienArray:
+            for alien in alienRow:
+                if bulletPosition.colliderect(alien.tiles.sprites()[0].rect):
+                    alienRow.remove(alien)
+                    print("Alien hit!")
+                    self.isShot = True
+                if self.isShot:
+                    alien.speed += 0.2
+                    self.isShot = False
+
 # have the aliens array declared in a method here, maybe take in the parameters for the offsets
+
 
 # # movement
 # shooting
